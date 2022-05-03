dfc2a317f722b238593e78216873cf805a1684a1
didim99 <didim@eclabs.ru>
2022-05-03 15:39:55 +0300

    + Introduced Main class  > Improved console output

--- main.py
+++ main.py
@@ -5,46 +5,64 @@ from driver import USBMeter
 from protocol import KnownDevice, HIDPacket, Command
 
 
-last_packet = 0
-millis = 0
+class Main:
+    millis: int
+    packet_cnt: int
+    time_start: float
+    last_packet: float
 
+    running: bool
 
-def on_packet(packet: HIDPacket):
-    if packet.payload.command != Command.DAT_RECV:
-        return
+    def on_packet(self, packet: HIDPacket) -> None:
+        if not self.running:
+            return
+        if packet.payload.command != Command.DAT_RECV:
+            return
 
-    global last_packet, millis
-    delta = int(round((time.time() - last_packet) * 1000.0))
-    last_packet = time.time()
-    millis += delta
-    if millis > 1000:
-        millis = 0
+        now = time.time()
+        total = (now - self.time_start) * 1000.0
+        delta = int(round((now - self.last_packet) * 1000.0))
+        self.last_packet = time.time()
+        self.millis += delta
+        if self.millis > 999:
+            self.millis = 0
 
-    data = packet.payload.data
-    run_time = str(timedelta(seconds=data.runTime))
-    sys.stdout.write("\r{:>8s}.{:03.0f} | V: {:6.3f} A: {:6.3f} D+: {:6.3f} D-: {:6.3f}"
-                     .format(run_time, millis, data.voltage, data.current, data.dp, data.dn))
-    sys.stdout.flush()
+        self.packet_cnt += 1
+        data = packet.payload.data
+        run_time = str(timedelta(seconds=data.runTime))
+        total = str(timedelta(milliseconds=total))[:-3]
+        sys.stdout.write(
+            "\r{:>8d} | {:>12s} | {:>8s}.{:03.0f} | {:6.3f} {:6.3f} | {:6.3f} {:6.3f}"
+            .format(self.packet_cnt, total, run_time, self.millis,
+                    data.voltage, data.current, data.dp, data.dn))
+        sys.stdout.flush()
 
+    def start(self) -> None:
+        input("Connect your device and press Enter")
 
-def start():
-    input("Connect your device and press Enter")
+        meter = USBMeter(KnownDevice.A2)
+        meter.recv_callback(self.on_packet)
+        meter.connect()
 
-    meter = USBMeter(KnownDevice.A2)
-    meter.recv_callback(on_packet)
-    meter.connect()
+        print("Press Enter to stop reading\n")
+        time.sleep(1)
 
-    print("Press Enter to stop reading")
-    time.sleep(3)
+        print("{:>8s} | {:>12s} | {:>12s} | {:>6s} {:>6s} | {:>6s} {:>6s}"
+              .format('packets', 'rectime', 'uptime', 'V', 'A', 'D+', 'D-'))
 
-    meter.start_read()
-    global last_packet
-    last_packet = time.time()
-    input()
-    meter.stop_read()
-    time.sleep(0.5)
-    print("\nGood bye!")
+        self.running = True
+        meter.start_read()
+        self.last_packet = time.time()
+        self.time_start = time.time()
+        self.packet_cnt = 0
+        self.millis = 0
+
+        input()
+        self.running = False
+        meter.stop_read()
+        time.sleep(0.5)
+        print("\nGood bye!")
 
 
 if __name__ == '__main__':
-    start()
+    Main().start()
